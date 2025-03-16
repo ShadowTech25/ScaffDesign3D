1f32276b3b1d77388ddf617b10bb15805b1f52c5
ShadowTech25 <johnnysouth1994@gmail.com>
2025-03-16 04:12:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ScaffDesign</h1>
+      <p>Your scaffolding design tool is under construction.</p>
+    </div>
+  );
+}
+
+export default App;
