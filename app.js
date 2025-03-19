90c70f8170baa5b5cf00c1cdd9bed8990811cb65
ShadowTech25 <johnnysouth1994@gmail.com>
2025-03-19 16:01:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 // Handle all GET requests by serving the React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-// Start the backend server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+})
